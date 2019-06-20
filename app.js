27261c9d06f475eb6682d40700baabc248aa75a5
Aaron Billings <asbillings2007@gmail.com>
2019-06-20 18:54:31 -0400

    created book routes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-import express from "express";
+const express = require("express");
 const app = express();
 
 // view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/", routes);
+app.use("/books", books);
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on server 3000!");
+});
--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,53 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 
+const Book = require("../models").Book;
+
 //get /books - Shows the full list of books.
-router.get((req, res) => {});
+router.get("/books", (req, res) => {
+  Book.findAll().then(books => {
+    res.render("", { books: books, title: "My Library!" });
+  });
+});
 //get /books/new - Shows the create new book form.
-router.get((req, res) => {});
+router.get("/books/new", (req, res) => {
+  render("books/new", { book: Book.build(), title: "New Book" });
+});
 //post /books/new - Posts a new book to the database.
-router.post((req, res) => {});
+router.post("/books/new", (req, res) => {
+  Book.create(req.body).then(book => {
+    res.redirect(`/books/${books.id}`);
+  });
+});
 //get /books/:id - Shows book detail form.
-router.get((req, res) => {});
+router.get("/books/:id", (req, res) => {
+  Book.findByPk(req.params.id).then(book => {
+    render("books/show", {
+      book: book,
+      title: book.title
+    });
+  });
+});
 //post /books/:id - Updates book info in the database.
-router.post((req, res) => {});
+router.put("/books/:id", (req, res) => {
+  Book.findByPk(req.params.id)
+    .then(book => {
+      return book.update(req.body);
+    })
+    .then(book => {
+      res.redirect(`/books/${book.id}`);
+    });
+});
 
 //post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
-router.post((req, res) => {});
+router.post("/books/:id/delete", (req, res) => {
+  Book.findByPk(req.params.id)
+    .then(book => {
+      return book.destroy();
+    })
+    .then(() => {
+      res.redirect("/books");
+    });
+});
+
+module.exports = router;
