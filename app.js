dd2d3e6235ea5a75c6ceceea6230510334cd547f
Aaron Billings <asbillings2007@gmail.com>
2019-06-25 11:01:56 -0400

    fixed search issues

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const sequelize = require("./models").sequelize;
-const Op = sequelize.Op;
 const bookRoutes = require("./routes/books");
 const mainRoute = require("./routes/index");
 const bodyParser = require("body-parser");
 // view engine
 
 app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use("/static", express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
